ee34db9dd5eef8c29bc201e7dcf89b3a3cb335d5
ritesh8380 <pritesh8380@gmail.com>
2024-08-29 11:25:47 +0530

    do while looped prime identifier

---
+++ dwprime.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+    int number,divisor,is_prime;
+    is_prime = 1;
+    printf("Chose the input for is_prime number test \t");
+    scanf("%d",&number);
+    if (number <= 1)
+    {
+        is_prime = 0;
+    }
+        divisor = 2;
+        do
+        { if (number%divisor == 0)
+        {
+          is_prime = 0;
+          break;
+        }
+        divisor++;
+        
+        } 
+        while (divisor < number);
+        if (is_prime)
+        {
+            printf("%d is a prime number \n",number);
+        }
+        else {
+            printf("%d is not a prime number \n",number);
+        } 
+        
+    return 0;
+} 
