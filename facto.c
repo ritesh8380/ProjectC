e7f94684e7a95f47e2bf82cdc4b21f60a2f46257
ritesh8380 <pritesh8380@gmail.com>
2024-09-01 12:51:54 +0530

    Factorial using loop

--- facto.c
+++ facto.c
@@ -16,5 +16,4 @@ return 0;
 /*Explaination
 
 1. Here product is initialized to 1 not 0 because if we do product with zero we will empty handed and we will get a garbage value.
-2.then a initialization is important then in loop iteration we wrote for product will be multiplied by a which is an increment.
-*/
+2.then a initialization is important then in loop iteration we wrote for product will be multiplied by a which is an increment. */
